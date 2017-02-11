0b88f91c66e438c53a7da8556ae2a372c337ec3a
Chris Davies <chriskd662@gmail.com>
2017-02-10 20:06:20 -0800

    switched asyncServer and asyncClient

--- asyncClient.py
+++ asyncClient.py
@@ -1,17 +1,1 @@
-#!usr/bin/python
-loadData = open("persistent.sfs")
-
-for line in loadData:
-	if line == "\tFLIGHTSTATE\n":
-		for line in loadData:
-			if line == "\t\tVESSEL\n":
-				#do something here to the vessel
-				for line in loadData:
-					print line,
-					if line == "\t\t}\n":
-						break
-
-			elif line == "	}\n":
-				break
-
-loadData.close()
+#usr/bin/python
