de42484dfe86bde2b274277bd28f3d7b4d7ed886
Chris Davies <chriskd662@gmail.com>
2017-02-10 20:04:44 -0800

    prints flight data

--- asyncClient.py
+++ asyncClient.py
@@ -1,11 +1,17 @@
 #!usr/bin/python
 loadData = open("persistent.sfs")
-for line in loadData:
-	if line == "	FLIGHTSTATE\n":
-		for line in loadData:
-			if line == "		VESSEL\n":
-				#do something here to the vessel
 
+for line in loadData:
+	if line == "\tFLIGHTSTATE\n":
+		for line in loadData:
+			if line == "\t\tVESSEL\n":
+				#do something here to the vessel
+				for line in loadData:
+					print line,
+					if line == "\t\t}\n":
+						break
 
 			elif line == "	}\n":
 				break
+
+loadData.close()
