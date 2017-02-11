3b53679ec0b667e4645478b60b82801d61c7f6f2
Brennan Wilkes <brennan@codexwilkes.com>
2017-02-10 20:12:09 -0800

    Basic asyncclient

--- asyncClient.py
+++ asyncClient.py
@@ -1,1 +1,24 @@
 #usr/bin/python
+import socket
+import sys
+
+
+#create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#now connect to the web server on port 80
+# - the normal http port
+s.connect(("pi.codexwilkes.com", 8988))
+
+# Send data
+message = 'This is the message.  It will be repeated.'
+s.sendall(message)
+
+
+while data!="end":
+	data = s.recv(2048)
+	#amount_received += len(data)
+	#print >>sys.stderr, 'received "%s"' % data
+
+print 'closing socket'
+s.close()
