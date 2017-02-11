6c1a66067210c37593635ca67505ebf3d5aa8c91
Chris Davies <chriskd662@gmail.com>
2017-02-10 22:18:26 -0800

    don't push me

--- asyncServer.py
+++ asyncServer.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 
-def fillTree(fileData, depth):
+def fillTree(fileData, depth = 0):
+	'''makes a tree based on the data in persistent. We use this tree to extract values.'''
 	tree = []
 	for line in fileData:
 		if line[depth:-1] == '{':
@@ -10,12 +11,18 @@ def fillTree(fileData, depth):
 		else:
 			tree.append(line[depth:-1])
 	return tree
-def printTree(tree, depth):
+
+def printTree(tree, depth = 0):
+	'''prints the given tree, with indentation to represent depth.'''
 	for node in tree:
 		if type(node) == str:
 			print depth*'\t' + node
 		else:
 			printTree(node, depth+1)
+
+def getFromTree(tree, parents):
+	'''gets the subtree from a tree. Parents is a list of '''
+
 
 #main
 #make backup of data
@@ -27,10 +34,11 @@ oldData.close()
 
 #make server tree
 serverData = open("server.sfs")
-serverGraph = fillTree(serverData, 0)
+serverGraph = fillTree(serverData)
 serverData.close()
 
 #make client tree
 clientData = open("persistent.sfs")
-clientGraph = fillTree(clientData, 0)
+clientGraph = fillTree(clientData)
 clientData.close()
+
