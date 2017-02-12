53b8b20cef71b9ed41e39e7390a8ba8042c142c7
Chris Davies <chriskd662@gmail.com>
2017-02-11 20:46:00 -0800

    whoops, fixed

--- kspLib.py
+++ kspLib.py
@@ -75,8 +75,6 @@ def getFromTree(tree, parents):
 			newTree.append(getFromTree(tree[i+1], parents[1:]))
 	return newTree
 
-
-
 def remove_outer(L):
 	if len(L)==1:
 		return remove_outer(L[0])
@@ -105,5 +103,5 @@ def getPID(vessel):
 	'''given a vessel (represented as a list), return its pid.'''
 	for line in vessel:
 		if line.split(' = ')[0] == 'pid':
-			return line.split(' = ')[-1]
+			return int(line.split(' = ')[-1])
 	return -1
