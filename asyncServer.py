8c1f3c1ad6150733cae1366977a1ae271ef67418
Chris Davies <chriskd662@gmail.com>
2017-02-12 22:17:28 -0800

    socket thing

--- asyncServer.py
+++ asyncServer.py
@@ -1,128 +1,131 @@
 #!usr/bin/python
-
-import cPickle as pickle
-#import pickle
-from kspLib import *
-import socket
-import sys
-
-
-#try to load shipVers and serverGraph from file
 try:
-	saveData = open("serverSave.pkl")
-	serverGraph, shipVers = pickle.load(saveData)
-	saveData.close()
-except:
-	serverGraph = []
-	shipVers = {}
+	import cPickle as pickle
+	#import pickle
+	from kspLib import *
+	import socket
+	import sys
 
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((socket.gethostname(), 8988))
-serversocket.listen(5)
+	#try to load shipVers and serverGraph from file
+	try:
+		saveData = open("serverSave.pkl")
+		serverGraph, shipVers = pickle.load(saveData)
+		saveData.close()
+	except:
+		serverGraph = []
+		shipVers = {}
 
-while True:
-	
-	print "waiting for connection"
-	
-	#accept connections from outside
-	connection, client_address = serversocket.accept()
-	
-	print 'connection from',client_address
-	
-	totalData=""
-	
-	# Receive the data in small chunks and retransmit it
-	totalData=""
+
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind((socket.gethostname(), 8988))
+	serversocket.listen(5)
+
 	while True:
-		data = connection.recv(2048)
-		#print data,"\n\n\n\n\n"
-		#if data[-7:]!="abcdefg":
-		totalData+=data
-		if totalData[-7:]=="abcdefg":
-			break
-	totalData=totalData[:-7]
-	#print totalData
-	
-	#totalData = connection.recv(81920)
-	#print totalData
-	
+		
+		print "waiting for connection"
+		
+		#accept connections from outside
+		connection, client_address = serversocket.accept()
+		
+		print 'connection from',client_address
+		
+		totalData=""
+		
+		# Receive the data in small chunks and retransmit it
+		totalData=""
+		while True:
+			data = connection.recv(2048)
+			#print data,"\n\n\n\n\n"
+			#if data[-7:]!="abcdefg":
+			totalData+=data
+			if totalData[-7:]=="abcdefg":
+				break
+		totalData=totalData[:-7]
+		#print totalData
+		
+		#totalData = connection.recv(81920)
+		#print totalData
+		
 
-	print "data recieved"
-	
-	clientGraph = fillTree(totalData)
-	clientGraphReduced = getFromTree(clientGraph, ["GAME", "FLIGHTSTATE", "VESSEL"])
-	#clientGraphKerbal = getFromTree(clientGraph, ["GAME", "ROSTER", "KERBAL"])
-	clientGraphKerbal=[]
-	
-	print "Data graphed"
-	
-	#print clientGraphReduced
-	#print clientGraphReduced
-	#break
-	
-	
-	#FLIGHTSTATE
-	for i in range(len(clientGraphReduced)):
-		#print i
-		#print clientGraphReduced[i]
-		pid=getPID(clientGraphReduced[i])
-		#print pid
-		serverInd = find_ind(pid,serverGraph)
-		if (serverInd==-1):
-			#print "hi"
-			#not in server
-			serverGraph.append(clientGraphReduced[i])
-			shipVers[pid] = [client_address]
-		else:
-			#if client is up to date:
-			if client_address in shipVers[pid]:
-				#check if they are the same
-				if compare_tree(clientGraphReduced[i], serverGraph[serverInd]):
-					#print "yo"
-					serverGraph[serverInd] = clientGraphReduced[i]
-					shipVers[pid] = [client_address]
+		print "data recieved"
+		
+		clientGraph = fillTree(totalData)
+		clientGraphReduced = getFromTree(clientGraph, ["GAME", "FLIGHTSTATE", "VESSEL"])
+		#clientGraphKerbal = getFromTree(clientGraph, ["GAME", "ROSTER", "KERBAL"])
+		clientGraphKerbal=[]
+		
+		print "Data graphed"
+		
+		#print clientGraphReduced
+		#print clientGraphReduced
+		#break
+		
+		
+		#FLIGHTSTATE
+		for i in range(len(clientGraphReduced)):
+			#print i
+			#print clientGraphReduced[i]
+			pid=getPID(clientGraphReduced[i])
+			#print pid
+			serverInd = find_ind(pid,serverGraph)
+			if (serverInd==-1):
+				#print "hi"
+				#not in server
+				serverGraph.append(clientGraphReduced[i])
+				shipVers[pid] = [client_address]
 			else:
-				#because we're updating the client
-				shipVers[pid].append(client_address)
-	
-	print "Flightstate handled"
-	
-	#ROSTER
-	for i in range(len(clientGraphKerbal)):
-		name=get_name(clientGraphKerbal[i])
-		serverInd = find_k_ind(name,serverGraph)
-		if (serverInd==-1):
-			#print "hi1"
-			#not in server
-			serverGraph.append(clientGraphKerbal[i])
-			shipVers[name] = [client_address]
-		else:
-			#if client is up to date:
-			if client_address in shipVers[name]:
-				#check if they are the same
-				if compare_tree(clientGraphKerbal[i], serverGraph[serverInd]):
-					#print "yo1"
-					serverGraph[serverInd] = clientGraphKerbal[i]
-					shipVers[name] = [client_address]
+				#if client is up to date:
+				if client_address in shipVers[pid]:
+					#check if they are the same
+					if compare_tree(clientGraphReduced[i], serverGraph[serverInd]):
+						#print "yo"
+						serverGraph[serverInd] = clientGraphReduced[i]
+						shipVers[pid] = [client_address]
+				else:
+					#because we're updating the client
+					shipVers[pid].append(client_address)
+		
+		print "Flightstate handled"
+		
+		#ROSTER
+		for i in range(len(clientGraphKerbal)):
+			name=get_name(clientGraphKerbal[i])
+			serverInd = find_k_ind(name,serverGraph)
+			if (serverInd==-1):
+				#print "hi1"
+				#not in server
+				serverGraph.append(clientGraphKerbal[i])
+				shipVers[name] = [client_address]
 			else:
-				#because we're updating the client
-				shipVers[name].append(client_address)
-	
-	print "Roster handled"
-	
-	#print serverGraph
-	
-	#this is where we send the stuff back
-	print "sending data"
-	returndata=pickle.dumps(serverGraph)
-	#print returndata
-	connection.sendall(returndata)
-	connection.sendall('abcdefg')
-	print "data sent"
-	
+				#if client is up to date:
+				if client_address in shipVers[name]:
+					#check if they are the same
+					if compare_tree(clientGraphKerbal[i], serverGraph[serverInd]):
+						#print "yo1"
+						serverGraph[serverInd] = clientGraphKerbal[i]
+						shipVers[name] = [client_address]
+				else:
+					#because we're updating the client
+					shipVers[name].append(client_address)
+		
+		print "Roster handled"
+		
+		#print serverGraph
+		
+		#this is where we send the stuff back
+		print "sending data"
+		returndata=pickle.dumps(serverGraph)
+		#print returndata
+		connection.sendall(returndata)
+		connection.sendall('abcdefg')
+		print "data sent"
+		serversocket.close()
 
-	saveData = open("serverSave.pkl", 'w')
-	pickle.dump((serverGraph, shipVers), saveData)
-	saveData.close()
-	
+		saveData = open("serverSave.pkl", 'w')
+		pickle.dump((serverGraph, shipVers), saveData)
+		saveData.close()
+		
+except Exception as exc:
+	print type(exc), exc
+	serversocket.close()
